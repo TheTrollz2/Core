0ed76f8612694aa263466d6fa21efae1fe430d95
TheTrollz <jordan@theosterbergs.com>
2015-09-08 18:57:00 -0700

    Episode 20 - 9/8/15 at 7:00 PM PST
    Created Project Version 1.0

---
+++ src/com/thetrollzltd/minigame/Main.java
@@ -0,0 +1,18 @@
+package com.thetrollzltd.minigame;
+
+/**
+ * Created by TheTrollzYT. All rights reserved.
+ * Any person(s) using this code are subject to
+ * a $1000 fine if used without permissions.
+ */
+public class Main {
+
+    public void onEnable(){
+
+    }
+
+    public void onDisable(){
+
+    }
+
+}
